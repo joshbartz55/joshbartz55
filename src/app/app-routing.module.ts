bbcbcf4767b617d61f056d8fd500ddc78513aaa3
joshbartz55 <jobartz55@gmail.com>
2024-06-25 16:19:38 -0500

    retrying hash locations
    retrying hash locations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,19 +5,29 @@ import { SearchComponent } from './components/search/search.component';
 import { IgvComponent } from './components/igv/igv.component';
 import { MapsComponent } from './components/maps/maps.component';
 import { DocumentationComponent } from './components/documentation/documentation.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  {path: 'home', component: HomeComponent},
-  {path: 'search', component: SearchComponent},
-  {path: 'igv', component: IgvComponent},
-  {path: 'maps', component: MapsComponent},
-  {path: 'documentation', component: DocumentationComponent},
-  { path: '**', redirectTo: '/'}
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'igv', component: IgvComponent },
+  { path: 'maps', component: MapsComponent },
+  { path: 'documentation', component: DocumentationComponent },
+  { path: '**', redirectTo: '/home' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeComponent, SearchComponent, IgvComponent, MapsComponent, DocumentationComponent]
+
+export const routingComponents = [
+  HomeComponent, 
+  SearchComponent, 
+  IgvComponent, 
+  MapsComponent, 
+  DocumentationComponent
+];
